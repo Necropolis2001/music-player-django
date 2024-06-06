a56be81c4705e2443f6623366622943e6bd0f660
Necropolis2001 <62107871+Necropolis2001@users.noreply.github.com>
2024-06-07 05:27:34 +0530

    Add files via upload

---
+++ musicapp/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+# Add URLConf
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:song_id>/', views.detail, name='detail'),
+    path('mymusic/', views.mymusic, name='mymusic'),
+    path('playlist/', views.playlist, name='playlist'),
+    path('playlist/<str:playlist_name>/', views.playlist_songs, name='playlist_songs'),
+    path('favourite/', views.favourite, name='favourite'),
+    path('all_songs/', views.all_songs, name='all_songs'),
+    path('recent/', views.recent, name='recent'),
+    path('hindi_songs/', views.hindi_songs, name='hindi_songs'),
+    path('english_songs/', views.english_songs, name='english_songs'),
+    path('metal_songs/', views.metal_songs, name='metal_songs'),
+    path('rock_songs/', views.rock_songs, name='rock_songs'),
+    path('pop_songs/', views.pop_songs, name='pop_songs'),
+    path('play/<int:song_id>/', views.play_song, name='play_song'),
+    path('play_song/<int:song_id>/', views.play_song_index, name='play_song_index'),
+    path('play_recent_song/<int:song_id>/', views.play_recent_song, name='play_recent_song'),
+
+    
+
+]
